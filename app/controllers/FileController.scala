fda5779eb5c9ac1b915555736194c97cabd03b05
La Musique <lamusique@users.noreply.github.com>
2019-10-17 16:10:41 +0900

    Add a change left long since.

--- app/controllers/FileController.scala
+++ app/controllers/FileController.scala
@@ -22,6 +22,35 @@ class FileController(
 
   def download(path: String, extensionHint: String = "xml", baseDir: String = "file") = Action { implicit request =>
 //  def download(path: String, inline: Boolean = true, extensionHint: String = "xml", baseDir: String = "file") = Action { implicit request =>
+
+    logger.debug(inspect(path))
+    logger.info(traceRequest("Received Request", request))
+
+    val maybeRootPath = config.getOptional[String]("spass.mapping.rootpath")
+    val mappingDir = maybeRootPath.map(File(_)).getOrElse(cwd / "mapping")
+    val method = request.method.toLowerCase
+    logger.info(inspect(method))
+    val methodDir = mappingDir / baseDir / method
+
+    // Assume a URI is /type[/type/..]/ID.
+    val split = path.split("/")
+    val uriDir = split.foldLeft(methodDir)((z, n) => z / n)
+
+    val allReqs = (uriDir / "requests").list(_.extension == Some("." + contentTypeToUse.ext)).toSeq
+
+
+    request.mediaType match {
+      case Some(mediaType) =>
+        ContentType.valueOf(mediaType.mediaSubType) match {
+          case Some(contentType@ContentType.XML) =>
+          case Some(contentType@ContentType.JSON) =>
+          case Some(contentType@ContentType.FormUrlEncoded) =>
+          case _ =>
+        }
+      case None => None
+    }
+
+
     // TODO
     //Ok("downloads.")
     //Ok.sendFile((pwd / "mapping" / "file" / "get" / "wiles" / "Ring-theoretic properties of certain Hecke algebras.pdff").toJava, inline = true)
